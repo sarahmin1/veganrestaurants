d983097cdf70f024c52528639b617baff57400ac
Sarah Min <sarah.min@journalism.cuny.edu>
2018-04-24 19:30:07 -0400

    first updates

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	$(".filter").on("change", function (e) {
+		e.preventDefault();
+
+		var filter = $(this).val();
+
+		$(".item.active").removeClass("active");
+
+		$("." + filter).addClass("active");
+	});
+});
